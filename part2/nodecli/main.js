00433bb547be3c984861c010ba1969c5fee6959a
komaitaira <komaitaira@gmail.com>
2021-01-19 21:39:26 +0900

    finish MarkdownをHTMLに変換する

--- part2/nodecli/main.js
+++ part2/nodecli/main.js
@@ -1,23 +1,35 @@
 // commanderモジュールをprogramオブジェクトとしてインポートする
 const program = require("commander");
 const fs = require("fs");
+// markedモジュールをmarkedオブジェクトとしてインポートする
+const marked = require("marked");
 
 
-// コマンドライン引数からファイルパスを取得する
+// gfmオプションを定義する
+program.option("--gfm", "GFMを有効にする");
 program.parse(process.argv);
 const filePath = program.args[0];
 
-// ファイルをUTF-8として非同期で読み込む
-fs.readFile(filePath, {encoding: "utf8"}, (err, file) => {
-  if (err) {
-    console.error(err.message);
-    // 終了ステータス1(一般的なエラー)としてプロセスを終了する
-    process.exit(1);
-    return;
-  }
-  console.log(file);
-});
+// コマンドライン引数のオプションを取得し、デフォルトのオプションを上書きする
+const cliOptions = {
+    gfm: false,
+    ...program.opts(),
+};
 
+
+// ファイルをUTF-8として非同期で読み込む
+fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
+  if (err) {
+      console.error(err.message);
+      process.exit(1);
+      return;
+  }
+  const html = marked(file, {
+      // オプションの値を使用する
+      gfm: cliOptions.gfm,
+  });
+  console.log(html);
+});
 // process.argv配列にnodeコマンドのコマンドライン引数が格納されていることを確認した
 // npmを使ってパッケージをインストールする方法を理解した
 // require関数を使ってパッケージのモジュールを読み込めることを確認した
@@ -28,3 +40,8 @@ fs.readFile(filePath, {encoding: "utf8"}, (err, file) => {
 // fsモジュールのreadFile関数を使ってファイルを読み込んだ
 // UTF-8形式のファイルの中身をコンソールに出力した
 // readFile関数の呼び出しにエラーハンドリング処理を記述した
+
+
+// markedパッケージを使ってMarkdown文字列をHTML文字列に変換した
+// コマンドライン引数でmarkedの変換オプションを設定した
+// デフォルトオプションを定義し、コマンドライン引数で上書きできるようにした
