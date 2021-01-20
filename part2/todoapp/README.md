582cb34b456d065854eb042e12e5b1772d2c5f94
komaitaira <komaitaira@gmail.com>
2021-01-20 16:31:42 +0900

    fix eventの設定、App.jsからのmountの呼び出し

---
+++ part2/todoapp/README.md
@@ -0,0 +1,18 @@
+# js-primer TodoApp
+
+# 起動
+```
+npx @js-primer/local-server
+```
+
+# ポイント
+Todoアプリでは、ユーザーが次のような操作した場合に、Todoアイテムを追加します。
+
+- 入力欄にTodoアイテムのタイトルを入力する
+- 入力欄でEnterキーを押して送信する
+- TodoリストにTodoアイテムが追加される
+これをJavaScriptで実現するには次のことが必要です。
+
+- Todoアイテムのタイトルを取得するために、input要素（入力欄）から内容を取得する
+- Enterキーで送信されたことを知るために、form要素のsubmitイベント（送信）を監視する
+- 入力内容をタイトルにしたTodoアイテムを作成し、Todoリスト（#js-todo-list）にTodoアイテム要素を追加する
--- part2/todoapp/src/App.js
+++ part2/todoapp/src/App.js
@@ -1,6 +1,11 @@
-console.log("App.js: loaded");
 export class App {
-  constructor() {
-    console.log("App initialized");
+  mount() {
+    const formElement = document.querySelector('#js-form');
+    const inputElement = document.querySelector('#js-form-input');
+    formElement.addEventListener('submit', (event) => {
+      // submitイベントの本来の動作を止める。フォームの内容を指定したURLへ送信するという動作をキャンセルする
+      event.preventDefault();
+      console.log(`入力値の値: ${inputElement.value}`);
+    });
   }
 }
